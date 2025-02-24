e8e570c24bd7e0757a45bc7400f6dc3ec781ec12
roser <roser_han@126.com>
2025-02-24 19:29:13 +0800

    - 新增 clang_format 配置。 - 完善 gitignore。 - 新增生成 1G 整数数据文件样例代码。

---
+++ io/app/generate_example_file.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    std::string file_name = "1g_int_example_file.dat";
+    std::ofstream file(file_name, std::ios::binary | std::ios::out);
+    if (!file.is_open()) {
+        std::cerr << "File open failed: " << file_name << std::endl;
+    }
+    std::cout << "Writing to the file..." << std::endl;
+
+    // Write data to the file.
+    // 1G file.
+    // Use a buffer to speed up the writing process.
+    constexpr int buffer_size = 1024 * 1024;
+    int* buffer = new int[buffer_size];
+    for (int i = 0; i < 1024 / sizeof(int); ++i) {
+        for (int i = 0; i < buffer_size; ++i) {
+            buffer[i] = rand();
+        }
+        file.write(reinterpret_cast<char*>(buffer), buffer_size * sizeof(int));
+    }
+
+    file.close();
+    std::cout << "Complete." << std::endl;
+    return 0;
+}
