d8b14643a909b99eb085115c6d23f99638322121
roser <roser_han@126.com>
2025-04-01 14:08:59 +0800

    大文件排序实现快速排序算法，写入文件并输出排序数组验证。

--- algorithms/sort/big_file_merge_sort/app/big_file_merge_sort.cpp
+++ algorithms/sort/big_file_merge_sort/app/big_file_merge_sort.cpp
@@ -1,15 +1,53 @@
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
 
-#include <queue>
-
-using std::priority_queue;
 using std::pair;
+using std::priority_queue;
 
-void quick_sort(const int* nums, int size)
+void partial()
 {
-    // Todo: Implement.
+}
+
+// The range is [begin, end).
+void quick_sort(const int* nums, int begin, int end)
+{
+    if (begin >= end) {
+        return;
+    }
+    int less = begin + 1;
+    int greater = end - 1;
+    int target = begin;
+    while (less < greater) {
+        while (less < greater && nums[less] <= nums[target]) {
+            less++;
+        }
+        while (less < greater && nums[greater] > nums[target]) {
+            greater--;
+        }
+
+        if (less < greater) {
+            std::swap(nums[less], nums[greater]);
+        }
+    }
+    std::swap(nums[less - 1], nums[target]);
+    target = less - 1;
+
+    quick_sort(nums, begin, target);
+    quick_sort(nums, target + 1, end);
+}
+
+void write_segment_to_file(const int* nums, int size, int file_num)
+{
+}
+
+void print_nums(const int* nums, int size)
+{
+    for (int i = 0; i < size; ++i) {
+        std::cout << nums[i] << ' ';
+    }
+    std::cout << '\n' << std::endl;
 }
 
 void read_and_sort()
@@ -23,15 +61,24 @@ void read_and_sort()
 
     // Read 256MB every time.
     int buffer_size = 256 * 1024 * 1024;
-    int size = buffer_size / sizeof(int);
-    int* num_buffer = new int[size];
-    int pos;
+    int* num_buffer = new int[buffer_size / sizeof(int)];
+    int position = 0;
     while (file.good()) {
         file.read(reinterpret_cast<char*>(num_buffer), buffer_size);
-        // Sort the buffer.
-        quick_sort(num_buffer, size);
 
-        // Todo: Write to a temporal data file.
+        // Update the file read position.
+        int read_size = file.gcount() - position;
+        int read_num_size = read_size / sizeof(int);
+        position = file.gcount();
+
+        // Sort the buffer.
+        quick_sort(num_buffer, 0, read_num_size);
+
+        // Debug: Print the sorted nums.
+        print_nums(num_buffer, read_num_size);
+        
+        // Write to a temporal data file.
+        write_segment_to_file(num_buffer, read_num_size, position);
     }
 
     delete[] num_buffer;
@@ -41,9 +88,9 @@ void read_and_sort()
 
 void multi_file_merge_sort()
 {
-
 }
 
-int main() {
+int main()
+{
     return 0;
 }
