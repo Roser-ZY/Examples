0c56e25dc66f8d12d578ae030b73149538e468ba
Roser <roser_han@126.com>
2025-08-19 21:32:13 +0800

    Math - 完善快速乘法负数版本，待验证。

--- math/quick_multiplier/quick_multiplier.cpp
+++ math/quick_multiplier/quick_multiplier.cpp
@@ -17,10 +17,26 @@ int quick_multiplier(int multiplier, int multiplicand)
     return result;
 }
 
+int quick_multiplier_with_negative(int multiplier, int multiplicand)
+{
+    int result = 0;
+    multiplicand--;
+    while (multiplicand < -1) {
+        if ((multiplicand & 1) == 0) {
+            result += multiplier;
+        }
+        // k >>= 1 is the same as k = k / 2
+        multiplicand >>= 1;
+        // Because k right shift 1 bit, a need to be added to itself(a * 2).
+        multiplier += multiplier;
+    }
+    return result;
+}
+
 int main()
 {
-    int a = 12837;
-    int b = 52;
+    int a = -6;
+    int b = -3;
     int result = quick_multiplier(a, b);
     std::cout << "result: " << result << std::endl;
     return 0;
